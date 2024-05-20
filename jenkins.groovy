5799f160b67a00326a855eebb6a9ad22b21614aa
Viktor Nikonenko <117480569+viktordiktor@users.noreply.github.com>
2024-05-21 00:22:11 +0300

    init: pipeline jenkins

---
+++ jenkins.groovy
@@ -0,0 +1,73 @@
+task_branch = "${TEST_BRANCH_NAME}"
+def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
+currentBuild.displayName = "$branch_cutted"
+base_git_url = "https://gitlab.com/viktordiktor/seleniumSwagLabs.git"
+
+
+node {
+    withEnv(["branch=${branch_cutted}", "base_url=${base_git_url}"]) {
+        stage("Checkout Branch") {
+            if (!"$branch_cutted".contains("master")) {
+                try {
+                    getProject("$base_git_url", "$branch_cutted")
+                } catch (err) {
+                    echo "Failed get branch $branch_cutted"
+                    throw ("${err}")
+                }
+            } else {
+                echo "Current branch is master"
+            }
+        }
+
+        try {
+            parallel getTestStages(["apiTests", "uiTests"])
+        } finally {
+            stage ("Allure") {
+                generateAllure()
+            }
+        }
+    }
+}
+
+
+def getTestStages(testTags) {
+    def stages = [:]
+    testTags.each { tag ->
+        stages["${tag}"] = {
+            runTestWithTag(tag)
+        }
+    }
+    return stages
+}
+
+
+def runTestWithTag(String tag) {
+    try {
+        labelledShell(label: "Run ${tag}", script: "clean test ${tag}")
+    } finally {
+        echo "some failed tests"
+    }
+}
+
+def getProject(String repo, String branch) {
+    cleanWs()
+    checkout scm: [
+            $class           : 'GitSCM', branches: [[name: branch]],
+            userRemoteConfigs: [[
+                                        url: repo
+                                ]]
+    ]
+}
+
+def generateAllure() {
+    allure([
+            includeProperties: true,
+            jdk              : '',
+            properties       : [],
+            reportBuildPolicy: 'ALWAYS',
+            results          : [[path: 'target/allure-results']]
+    ])
+}
+
+
+
